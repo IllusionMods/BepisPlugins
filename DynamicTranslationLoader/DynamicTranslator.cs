cc002811d86a85dedaa704256649467c2f159a32
exdownloader <exdownloader@gmail.com>
2018-04-22 15:07:54 +0100

    Fix startup nullref.

--- DynamicTranslationLoader/DynamicTranslator.cs
+++ DynamicTranslationLoader/DynamicTranslator.cs
@@ -165,6 +165,7 @@ namespace DynamicTranslationLoader
 
         void Update()
         {
+            if (Event.current == null) return;
             if (UnityEngine.Event.current.Equals(ReloadTranslationsKeyEvent))
             {
                 Retranslate();
