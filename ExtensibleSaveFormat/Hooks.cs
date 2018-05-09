250c4958dc2a81b6a61493fdb4e5d552a058a62d
Bepis <36346617+bbepis@users.noreply.github.com>
2018-05-10 07:24:34 +1000

    Rework extended save data to use vanilla data structure

--- ExtensibleSaveFormat/Hooks.cs
+++ ExtensibleSaveFormat/Hooks.cs
@@ -3,13 +3,17 @@ using MessagePack;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
 
 namespace ExtensibleSaveFormat
 {
 	public static class Hooks
 	{
 		public static string Marker = "KKEx";
-		public static int Version = 2;
+		public static int Version = 3;
 
 		public static void InstallHooks()
 		{
@@ -17,81 +21,203 @@ namespace ExtensibleSaveFormat
 			harmony.PatchAll(typeof(Hooks));
 		}
 
-		[HarmonyPostfix, HarmonyPatch(typeof(ChaFile), "SaveFile", new[] { typeof(BinaryWriter), typeof(bool) })]
-		public static void SaveFileHook(ChaFile __instance, bool __result, BinaryWriter bw, bool savePng)
-		{
-			if (!__result)
-				return;
 
-			ExtendedSave.writeEvent(__instance);
+        #region Saving
+        private static byte[] currentlySavingData = null;
 
-			Dictionary<string, PluginData> extendedData = ExtendedSave.GetAllExtendedData(__instance);
-			if (extendedData == null)
-				return;
+	    public static void SaveFileHook(ChaFile file, BlockHeader header, ref long[] array3)
+	    {
+            ExtendedSave.writeEvent(file);
 
-			byte[] bytes = MessagePackSerializer.Serialize(extendedData);
+            Dictionary<string, PluginData> extendedData = ExtendedSave.GetAllExtendedData(file);
+	        if (extendedData == null)
+	        {
+	            currentlySavingData = null;
+	            return;
+	        }
 
-			bw.Write(Marker);
-			bw.Write(Version);
-			foreach (KeyValuePair<string, PluginData> kv in extendedData)
-			{
-				PluginData dict = kv.Value as PluginData;
-			}
+	        currentlySavingData = MessagePackSerializer.Serialize(extendedData);
 
-			bw.Write((int) bytes.Length);
-			bw.Write(bytes);
-		}
+            //get offset
+	        long offset = array3.Sum();
+	        long length = currentlySavingData.LongLength;
 
-		[HarmonyPostfix, HarmonyPatch(typeof(ChaFile), "LoadFile", new[] {typeof(BinaryReader), typeof(bool), typeof(bool)})]
-		public static void LoadFileHook(ChaFile __instance, bool __result, BinaryReader br, bool noLoadPNG, bool noLoadStatus)
-		{
-			Dictionary<string, PluginData> dictionary = null;
+            //insert our custom data length at the end
+            Array.Resize(ref array3, array3.Length + 1);
+	        array3[array3.Length - 1] = length;
 
-			if (!__result)
-				return;
+            //add info about our data to the block header
+	        BlockHeader.Info info = new BlockHeader.Info
+	        {
+                name = Marker,
+                version = Version.ToString(),
+                pos = offset,
+                size = length
+	        };
 
-			if (br.BaseStream.Position != br.BaseStream.Length)
-			{
-			    long originalPosition = br.BaseStream.Position;
+	        header.lstInfo.Add(info);
+	    }
 
-				try
-				{
-					string marker = br.ReadString();
-					int version = br.ReadInt32();
+        [HarmonyPostfix, HarmonyPatch(typeof(ChaFile), "SaveFile", new[] { typeof(BinaryWriter), typeof(bool) })]
+        public static void SaveFilePostHook(ChaFile __instance, bool __result, BinaryWriter bw, bool savePng)
+        {
+            if (!__result || currentlySavingData == null)
+                return;
+            
+            bw.Write(currentlySavingData);
+        }
 
-					if (marker == Marker && version == Version)
-					{
-						int length = br.ReadInt32();
+        [HarmonyTranspiler, HarmonyPatch(typeof(ChaFile), "SaveFile", new[] { typeof(BinaryWriter), typeof(bool) })]
+        public static IEnumerable<CodeInstruction> SaveFileTranspiler(IEnumerable<CodeInstruction> instructions)
+	    {
+            List<CodeInstruction> newInstructionSet = new List<CodeInstruction>(instructions);
 
-						if (length > 0)
-						{
-							byte[] bytes = br.ReadBytes(length);
-							dictionary = MessagePackSerializer.Deserialize<Dictionary<string, PluginData>>(bytes);
-						}
-					}
-					else
-					{
-					    br.BaseStream.Position = originalPosition;
-					}
-				}
-				catch (EndOfStreamException)
-				{
-					/* Incomplete/non-existant data */
-				}
-				catch (InvalidOperationException)
-				{
-					/* Invalid/unexpected deserialized data */
-				}
-			}
+            //get the index of the last blockheader creation
+	        int blockHeaderIndex = newInstructionSet.FindLastIndex(instruction => CheckNewObjTypeName(instruction, "BlockHeader"));
 
-			if (dictionary == null)
-			{
-				//initialize a new dictionary since it doesn't exist
-				dictionary = new Dictionary<string, PluginData>();
-			}
+            //get the index of array3 (which contains data length info)
+            int array3Index = newInstructionSet.FindIndex(instruction =>
+            {
+                //find first int64 array
+                return instruction.opcode == OpCodes.Newarr &&
+                       instruction.operand.ToString() == "System.Int64";
+            });
 
-			ExtendedSave.internalDictionary.Set(__instance, dictionary);
-			ExtendedSave.readEvent(__instance);
-		}
+            LocalBuilder blockHeaderLocalBuilder = (LocalBuilder)newInstructionSet[blockHeaderIndex + 1].operand; //get the local index for the block header
+	        LocalBuilder array3LocalBuilder = (LocalBuilder)newInstructionSet[array3Index + 1].operand; //get the local index for array3
+
+            //insert our own hook right after the blockheader creation
+            newInstructionSet.InsertRange(blockHeaderIndex + 2, //we insert AFTER the NEXT instruction, which is the store local for the blockheader
+	            new[] {
+	                new CodeInstruction(OpCodes.Ldarg_0), //push the ChaFile instance
+	                new CodeInstruction(OpCodes.Ldloc_S, blockHeaderLocalBuilder), //push the BlockHeader instance 
+	                new CodeInstruction(OpCodes.Ldloca_S, array3LocalBuilder), //push the array3 instance as ref
+                    new CodeInstruction(OpCodes.Call, typeof(Hooks).GetMethod(nameof(SaveFileHook))), //call our hook
+	            });
+
+	        return newInstructionSet;
+	    }
+        #endregion
+
+        #region Loading
+
+	    public static void LoadFileHook(ChaFile file, BlockHeader header, BinaryReader reader)
+	    {
+	        var info = header.SearchInfo(Marker);
+
+	        if (info != null && info.version == Version.ToString())
+	        {
+	            long originalPosition = reader.BaseStream.Position;
+	            long basePosition = originalPosition - header.lstInfo.Sum(x => x.size);
+
+	            reader.BaseStream.Position = basePosition + info.pos;
+
+
+	            byte[] data = reader.ReadBytes((int)info.size);
+
+	            reader.BaseStream.Position = originalPosition;
+
+
+	            var dictionary = MessagePackSerializer.Deserialize<Dictionary<string, PluginData>>(data);
+
+	            ExtendedSave.internalDictionary.Set(file, dictionary);
+	        }
+	        else
+	        {
+	            ExtendedSave.internalDictionary.Set(file, new Dictionary<string, PluginData>());
+	        }
+	    }
+
+        [HarmonyTranspiler, HarmonyPatch(typeof(ChaFile), "LoadFile", new[] { typeof(BinaryReader), typeof(bool), typeof(bool) })]
+        public static IEnumerable<CodeInstruction> LoadFileTranspiler(IEnumerable<CodeInstruction> instructions)
+	    {
+            List<CodeInstruction> newInstructionSet = new List<CodeInstruction>(instructions);
+
+            //get the index of the first searchinfo call
+	        int searchInfoIndex = newInstructionSet.FindIndex(instruction => CheckCallVirtName(instruction, "SearchInfo"));
+
+	        //get the index of the last seek call
+	        int lastSeekIndex = newInstructionSet.FindLastIndex(instruction => CheckCallVirtName(instruction, "Seek"));
+
+            LocalBuilder blockHeaderLocalBuilder = (LocalBuilder)newInstructionSet[searchInfoIndex - 2].operand; //get the localbuilder for the blockheader
+
+            //insert our own hook right after the last seek
+            newInstructionSet.InsertRange(lastSeekIndex + 2, //we insert AFTER the NEXT instruction, which is right before the try block exit
+                new[] {
+                    new CodeInstruction(OpCodes.Ldarg_0), //push the ChaFile instance
+                    new CodeInstruction(OpCodes.Ldloc_S, blockHeaderLocalBuilder), //push the BlockHeader instance
+                    new CodeInstruction(OpCodes.Ldarg_1, blockHeaderLocalBuilder), //push the binaryreader instance
+                        new CodeInstruction(OpCodes.Call, typeof(Hooks).GetMethod(nameof(LoadFileHook))), //call our hook
+                });
+
+            return newInstructionSet;
+	    }
+
+	    [HarmonyPostfix, HarmonyPatch(typeof(ChaFile), "LoadFile", new[] { typeof(BinaryReader), typeof(bool), typeof(bool) })]
+	    public static void LoadFilePostHook(ChaFile __instance, bool __result, BinaryReader br, bool noLoadPNG, bool noLoadStatus)
+	    {
+	        if (!__result)
+	            return;
+
+
+            //Compatibility for ver 1 and 2 ext save data
+	        if (br.BaseStream.Position != br.BaseStream.Length)
+	        {
+	            long originalPosition = br.BaseStream.Position;
+
+	            try
+	            {
+	                string marker = br.ReadString();
+	                int version = br.ReadInt32();
+
+	                if (marker == "KKEx" && version == 2)
+	                {
+	                    int length = br.ReadInt32();
+
+	                    if (length > 0)
+	                    {
+	                        byte[] bytes = br.ReadBytes(length);
+	                        var dictionary = MessagePackSerializer.Deserialize<Dictionary<string, PluginData>>(bytes);
+
+	                        ExtendedSave.internalDictionary.Set(__instance, dictionary);
+	                    }
+	                }
+	                else
+	                {
+	                    br.BaseStream.Position = originalPosition;
+	                }
+	            }
+	            catch (EndOfStreamException)
+	            {
+	                /* Incomplete/non-existant data */
+	            }
+	            catch (InvalidOperationException)
+	            {
+	                /* Invalid/unexpected deserialized data */
+	            }
+	        }
+	        
+	        ExtendedSave.readEvent(__instance);
+	    }
+	    #endregion
+
+        
+	    #region Helper
+
+	    public static bool CheckCallVirtName(CodeInstruction instruction, string name)
+	    {
+	        return instruction.opcode == OpCodes.Callvirt &&
+	               //need to do reflection fuckery here because we can't access MonoMethod which is the operand type, not MehtodInfo like normal reflection
+	               instruction.operand.GetType().GetProperty("Name", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).GetGetMethod().Invoke(instruction.operand, null).ToString().ToString() == name;
+	    }
+
+	    public static bool CheckNewObjTypeName(CodeInstruction instruction, string name)
+	    {
+	        return instruction.opcode == OpCodes.Newobj &&
+	               //need to do reflection fuckery here because we can't access MonoCMethod which is the operand type, not ConstructorInfo like normal reflection
+	               instruction.operand.GetType().GetProperty("DeclaringType", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).GetGetMethod().Invoke(instruction.operand, null).ToString() == name;
+	    }
+
+	    #endregion
 	}
 }
