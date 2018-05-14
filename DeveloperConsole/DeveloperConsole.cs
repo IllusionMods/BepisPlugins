f7999046307a958df91fad548b562683579ef84a
MarC0 <dummyaccx@gmail.com>
2018-05-14 19:41:42 +0200

    Converted console to GUILayout, added scrolling, improved looks, improved trimming, longer buffer, added clear button

--- DeveloperConsole/DeveloperConsole.cs
+++ DeveloperConsole/DeveloperConsole.cs
@@ -1,29 +1,37 @@
 ﻿using BepInEx;
-using System;
 using UnityEngine;
+using System.Linq;
 
 namespace DeveloperConsole
 {
     [BepInPlugin(GUID: "com.bepis.bepinex.developerconsole", Name: "Developer Console", Version: "1.0.1")]
     public class DeveloperConsole : BaseUnityPlugin
     {
-        private Rect UI = new Rect(20, 20, 400, 200);
-        bool showingUI = false;
-        string TotalLog = "";
+        private int showCounter = 0;
+        private bool showingUI = false;
+        private string TotalLog = "";
+        private string TotalShowingLog = "";
+        private Rect UI = new Rect(20, 20, 400, 400);
+        private Vector2 scrollPosition = Vector2.zero;
 
-        int showCounter = 0;
-        string TotalShowingLog = "";
-
-        void Awake()
+        private void Awake()
         {
             BepInLogger.EntryLogged += (log, show) =>
             {
                 string current = $"{TotalLog}\r\n{log}";
-                if (current.Length > 2000)
+                if (current.Length > 3000)
                 {
-                    current = current.Remove(0, 1000);
+                    var trimmed = current.Remove(0, 500);
+
+                    // Trim until the first newline to avoid partial line
+                    var newlineHit = false;
+                    trimmed = new string(trimmed.SkipWhile(x => !newlineHit && !(newlineHit = (x == '\n'))).ToArray());
+
+                    current = "--LOG TRIMMED--\n" + trimmed;
                 }
                 TotalLog = current;
+
+                scrollPosition = new Vector2(0, float.MaxValue);
 
                 if (show)
                 {
@@ -36,27 +44,15 @@ namespace DeveloperConsole
             };
         }
 
-
-
-        void Update()
-        {
-            if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.F12))
-            {
-                showingUI = !showingUI;
-            }
-        }
-
-        void OnGUI()
+        private void OnGUI()
         {
             ShowLog();
 
             if (showingUI)
-                UI = GUI.Window("com.bepis.bepinex.developerconsole".GetHashCode() + 0, UI, WindowFunction, "Developer Console");
+                UI = GUILayout.Window("com.bepis.bepinex.developerconsole".GetHashCode() + 0, UI, WindowFunction, "Developer Console");
         }
 
-        
-
-        void ShowLog()
+        private void ShowLog()
         {
             if (showCounter != 0)
             {
@@ -74,22 +70,39 @@ namespace DeveloperConsole
             }
         }
 
-        void WindowFunction(int windowID)
+        private void Update()
         {
-            GUI.Label(new Rect(10, 40, 380, 160), TotalLog, new GUIStyle
+            if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.F12))
             {
-                alignment = TextAnchor.LowerLeft,
-                wordWrap = true,
-                normal = new GUIStyleState
-                {
-                    textColor = Color.white
-                }
-            });
-
-            if (GUI.Button(new Rect(295, 20, 100, 20), "Dump scene"))
-            {
-                SceneDumper.DumpScene();
+                showingUI = !showingUI;
             }
+        }
+
+        private void WindowFunction(int windowID)
+        {
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button("Clear console"))
+                    TotalLog = "Log cleared";
+                if (GUILayout.Button("Dump scene"))
+                    SceneDumper.DumpScene();
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginVertical(GUI.skin.box);
+            {
+                scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
+                {
+                    GUILayout.BeginVertical();
+                    {
+                        GUILayout.FlexibleSpace();
+                        GUILayout.TextArea(TotalLog, GUI.skin.label);
+                    }
+                    GUILayout.EndVertical();
+                }
+                GUILayout.EndScrollView();
+            }
+            GUILayout.EndVertical();
 
             GUI.DragWindow();
         }
