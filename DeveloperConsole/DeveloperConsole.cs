89779f94928e5cdb6e9c33f03aeb3e4a7496ee6b
MarC0 <dummyaccx@gmail.com>
2018-07-27 21:16:30 +0200

    Added separate dummy plugin for BepInEx settings

--- DeveloperConsole/DeveloperConsole.cs
+++ DeveloperConsole/DeveloperConsole.cs
@@ -2,7 +2,6 @@
 using BepInEx;
 using UnityEngine;
 using System.Linq;
-using BepInEx.Logging;
 using Logger = BepInEx.Logger;
 
 namespace DeveloperConsole
@@ -59,9 +58,9 @@ namespace DeveloperConsole
                 if (GUILayout.Button("Dump scene"))
                     SceneDumper.DumpScene();
 
-                LogDebug = GUILayout.Toggle(LogDebug, "Debug");
-                LogInfo = GUILayout.Toggle(LogInfo, "Info");
-                LogUnity = GUILayout.Toggle(LogUnity, "Unity");
+                BepInExSettings.LogDebug = GUILayout.Toggle(BepInExSettings.LogDebug, "Debug");
+                BepInExSettings.LogInfo = GUILayout.Toggle(BepInExSettings.LogInfo, "Info");
+                BepInExSettings.LogUnity = GUILayout.Toggle(BepInExSettings.LogUnity, "Unity");
             }
             GUILayout.EndHorizontal();
 
@@ -83,54 +82,6 @@ namespace DeveloperConsole
             GUI.DragWindow();
         }
 
-        #region LogSettings
-        [DisplayName("Enable Unity message logging")]
-        [DefaultValue(false)]
-        [Description("Changes take effect after game restart.")]
-        [Category("Logging")]
-        public static bool LogUnity
-        {
-            get => bool.Parse(Config.GetEntry("chainloader-log-unity-messages", "false", "BepInEx"));
-            set => Config.SetEntry("chainloader-log-unity-messages", value.ToString(), "BepInEx");
-        }
-        
-        [DisplayName("Show system console")]
-        [DefaultValue(false)]
-        [Description("Changes take effect after game restart.")]
-        public static bool ShowConsole
-        {
-            get => bool.Parse(Config.GetEntry("console", "false", "BepInEx"));
-            set => Config.SetEntry("console", value.ToString(), "BepInEx");
-        }
-
-        [Browsable(true)]
-        [DisplayName("Enable DEBUG logging")]
-        [DefaultValue(false)]
-        [Category("Logging")]
-        public static bool LogDebug
-        {
-            get => GetDebugFlag(LogLevel.Debug);
-            set => SetDebugFlag(value, LogLevel.Debug);
-        }
-        [Browsable(true)]
-        [DisplayName("Enable INFO logging")]
-        [DefaultValue(false)]
-        [Category("Logging")]
-        public static bool LogInfo
-        {
-            get => GetDebugFlag(LogLevel.Info);
-            set => SetDebugFlag(value, LogLevel.Info);
-        }
-        [Browsable(true)]
-        [DisplayName("Enable MESSAGE logging")]
-        [DefaultValue(true)]
-        [Category("Logging")]
-        public static bool LogMessage
-        {
-            get => GetDebugFlag(LogLevel.Message);
-            set => SetDebugFlag(value, LogLevel.Message);
-        }
-
         [DisplayName("Size of the log buffer in characters")]
         [AcceptableValueRange(4000, 16300, false)]
         public ConfigWrapper<int> LogDepth { get; }
@@ -139,22 +90,5 @@ namespace DeveloperConsole
         {
             LogDepth = new ConfigWrapper<int>("LogDepth", this, 16300);
         }
-
-        private static void SetDebugFlag(bool value, LogLevel level)
-        {
-            if (value)
-                Logger.CurrentLogger.DisplayedLevels |= level;
-            else
-                Logger.CurrentLogger.DisplayedLevels &= ~level;
-
-            Config.SetEntry("logger-displayed-levels", Logger.CurrentLogger.DisplayedLevels.ToString(), "BepInEx");
-        }
-
-        private static bool GetDebugFlag(LogLevel level)
-        {
-            return (Logger.CurrentLogger.DisplayedLevels & level) == level;
-        }
-
-        #endregion
     }
 }
