c50157c141d3f11b807a70ccc7ad568159731a8f
MarC0 <dummyaccx@gmail.com>
2018-11-01 02:08:34 +0100

    Combine repeated messages; Fixed display time being fps-dependant; Longer display time for more messages

--- MessageCenter/MessageCenter.cs
+++ MessageCenter/MessageCenter.cs
@@ -1,63 +1,95 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using BepInEx;
 using BepInEx.Logging;
 using UnityEngine;
 
 namespace MessageCenter
 {
-    [BepInPlugin("com.bepis.messagecenter", "Message Center", "1.1")]
+    [BepInPlugin("com.bepis.messagecenter", "Message Center", "1.2")]
     public class MessageCenter : BaseUnityPlugin
     {
-        [DisplayName("Show messages in UI")]
-        [Description("Allows plugins to show pop-up messages")]
-        [Advanced(true)]
-        public ConfigWrapper<bool> Enabled { get; }
+        private readonly List<LogEntry> _shownLogLines = new List<LogEntry>();
+
+        private float _showCounter;
+        private string _shownLogText = string.Empty;
 
         public MessageCenter()
         {
             Enabled = new ConfigWrapper<bool>("enabled", this, true);
         }
 
-        private int showCounter = 0;
-        private string TotalShowingLog = string.Empty;
+        [DisplayName("Show messages in UI")]
+        [Description("Allow plugins to show pop-up messages")]
+        [Advanced(true)]
+        public ConfigWrapper<bool> Enabled { get; }
 
-        protected void Awake()
+        private void Awake()
         {
-            BepInEx.Logger.EntryLogged += (level, log) =>
-            {
-                if(Enabled.Value && (level & LogLevel.Message) != LogLevel.None)
-                {
-                    if(showCounter == 0)
-                        TotalShowingLog = string.Empty;
-
-                    showCounter = 600;
-                    TotalShowingLog = $"{log}\r\n{TotalShowingLog}";
-                }
-            };
+            BepInEx.Logger.EntryLogged += OnEntryLogged;
         }
 
-        protected void OnGUI()
+        private void OnEntryLogged(LogLevel level, object log)
         {
-            if(showCounter != 0)
+            if ((level & LogLevel.Message) != LogLevel.None && Enabled.Value)
             {
-                showCounter--;
+                if (_showCounter <= 0)
+                    _shownLogLines.Clear();
 
-                Color color = Color.white;
-                Color color2 = Color.black;
+                _showCounter = Mathf.Max(_showCounter, 7f);
 
-                if(showCounter < 100)
+                var logText = log.ToString();
+
+                var logEntry = _shownLogLines.FirstOrDefault(x => x.Text.Equals(logText, StringComparison.Ordinal));
+                if (logEntry == null)
                 {
-                    var a = (float)showCounter / 100;
-                    color.a = a;
-                    color2.a = a;
+                    logEntry = new LogEntry(logText);
+                    _shownLogLines.Add(logEntry);
+
+                    _showCounter = _showCounter + 0.8f;
                 }
 
-                ShadowAndOutline.DrawOutline(new Rect(40, 20, 600, 160), TotalShowingLog, new GUIStyle
+                logEntry.Count++;
+
+                var logLines = _shownLogLines.Select(x => x.Count > 1 ? $"{x.Count}x {x.Text}" : x.Text).ToArray();
+                _shownLogText = string.Join("\r\n", logLines);
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (_showCounter > 0)
+            {
+                _showCounter -= Time.deltaTime;
+
+                var textColor = Color.white;
+                var outlineColor = Color.black;
+
+                if (_showCounter <= 1)
+                {
+                    textColor.a = _showCounter;
+                    outlineColor.a = _showCounter;
+                }
+
+                ShadowAndOutline.DrawOutline(new Rect(40, 20, 600, 160), _shownLogText, new GUIStyle
                 {
                     alignment = TextAnchor.UpperLeft,
-                    fontSize = 20,
-                }, color2, color, 3f);
+                    fontSize = 20
+                }, outlineColor, textColor, 3f);
             }
+        }
+
+        private sealed class LogEntry
+        {
+            public LogEntry(string text)
+            {
+                Text = text;
+            }
+
+            public string Text { get; }
+            public int Count { get; set; }
         }
     }
 }
