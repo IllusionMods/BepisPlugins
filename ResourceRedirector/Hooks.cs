2b81182f04aeae791aec7e0a7e65fb6bcc4ee742
DeathWeasel1337 <37981563+DeathWeasel1337@users.noreply.github.com>
2018-11-03 16:07:22 -0500

    Move LoadAssetAsync hook to prefix
    Should be functionally equivalent except with less "Unable to open archive file" errors in log

--- ResourceRedirector/Hooks.cs
+++ ResourceRedirector/Hooks.cs
@@ -1,103 +1,101 @@
-﻿using Harmony;
+﻿using BepInEx;
+using BepInEx.Logging;
+using Logger = BepInEx.Logger;
+using Harmony;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using BepInEx;
 using UnityEngine;
 
 namespace ResourceRedirector
 {
-	static class Hooks
-	{
-		public static void InstallHooks()
-		{
-			var harmony = HarmonyInstance.Create("com.bepis.bepinex.resourceredirector");
-			harmony.PatchAll(typeof(Hooks));
-		}
+    static class Hooks
+    {
+        public static void InstallHooks()
+        {
+            var harmony = HarmonyInstance.Create("com.bepis.bepinex.resourceredirector");
+            harmony.PatchAll(typeof(Hooks));
+        }
 
-	    #region List Loading
-	    [HarmonyPrefix, HarmonyPatch(typeof(ChaListControl), nameof(ChaListControl.CheckItemID), new [] { typeof(int), typeof(int) })]
-	    public static bool CheckItemIDHook(int category, int id, ref byte __result, ChaListControl __instance)
-	    {
-	        int pid = ListLoader.CalculateGlobalID(category, id);
+        #region List Loading
+        [HarmonyPrefix, HarmonyPatch(typeof(ChaListControl), nameof(ChaListControl.CheckItemID), new[] { typeof(int), typeof(int) })]
+        public static bool CheckItemIDHook(int category, int id, ref byte __result, ChaListControl __instance)
+        {
+            int pid = ListLoader.CalculateGlobalID(category, id);
 
             byte result = __instance.CheckItemID(pid);
 
-	        if (result > 0)
-	        {
-	            //BepInLogger.Log($"CHECK {category} : {id} : {result}");
-	            __result = result;
-	            return false;
-	        }
+            if (result > 0)
+            {
+                //BepInLogger.Log($"CHECK {category} : {id} : {result}");
+                __result = result;
+                return false;
+            }
 
-	        return true;
-	    }
+            return true;
+        }
 
-	    [HarmonyPrefix, HarmonyPatch(typeof(ChaListControl), nameof(ChaListControl.AddItemID), new [] { typeof(int), typeof(int), typeof(byte) })]
-	    public static bool AddItemIDHook(int category, int id, byte flags, ChaListControl __instance)
-	    {
-	        int pid = ListLoader.CalculateGlobalID(category, id);
+        [HarmonyPrefix, HarmonyPatch(typeof(ChaListControl), nameof(ChaListControl.AddItemID), new[] { typeof(int), typeof(int), typeof(byte) })]
+        public static bool AddItemIDHook(int category, int id, byte flags, ChaListControl __instance)
+        {
+            int pid = ListLoader.CalculateGlobalID(category, id);
 
-	        byte result = __instance.CheckItemID(pid);
+            byte result = __instance.CheckItemID(pid);
 
-	        if (result > 0)
-	        {
+            if (result > 0)
+            {
                 //BepInLogger.Log($"ADD {category} : {id} : {result}");
-	            __instance.AddItemID(pid, flags);
-	            return false;
-	        }
+                __instance.AddItemID(pid, flags);
+                return false;
+            }
 
-	        return true;
-	    }
+            return true;
+        }
 
-		[HarmonyPostfix, HarmonyPatch(typeof(ChaListControl), "LoadListInfoAll")]
-		public static void LoadListInfoAllPostHook(ChaListControl __instance)
-		{
-			string listPath = Path.Combine(ResourceRedirector.EmulatedDir, @"list\characustom");
+        [HarmonyPostfix, HarmonyPatch(typeof(ChaListControl), "LoadListInfoAll")]
+        public static void LoadListInfoAllPostHook(ChaListControl __instance)
+        {
+            string listPath = Path.Combine(ResourceRedirector.EmulatedDir, @"list\characustom");
 
-			//BepInLogger.Log($"List directory exists? {Directory.Exists(listPath).ToString()}");
+            //BepInLogger.Log($"List directory exists? {Directory.Exists(listPath).ToString()}");
 
-			if (Directory.Exists(listPath))
-				foreach (string csvPath in Directory.GetFiles(listPath, "*.csv", SearchOption.AllDirectories))
-				{
-					//BepInLogger.Log($"Attempting load of: {csvPath}");
+            if (Directory.Exists(listPath))
+                foreach (string csvPath in Directory.GetFiles(listPath, "*.csv", SearchOption.AllDirectories))
+                {
+                    //BepInLogger.Log($"Attempting load of: {csvPath}");
 
-					var chaListData = ListLoader.LoadCSV(File.OpenRead(csvPath));
-					ListLoader.ExternalDataList.Add(chaListData);
+                    var chaListData = ListLoader.LoadCSV(File.OpenRead(csvPath));
+                    ListLoader.ExternalDataList.Add(chaListData);
 
-					//BepInLogger.Log($"Finished load of: {csvPath}");
-				}
+                    //BepInLogger.Log($"Finished load of: {csvPath}");
+                }
 
-			ListLoader.LoadAllLists(__instance);
-		}
-	    #endregion
+            ListLoader.LoadAllLists(__instance);
+        }
+        #endregion
 
         #region Asset Loading
-		[HarmonyPrefix, HarmonyPatch(typeof(AssetBundleManager), nameof(AssetBundleManager.LoadAsset), new[] {typeof(string), typeof(string), typeof(Type), typeof(string)})]
-		public static bool LoadAssetPostHook(ref AssetBundleLoadAssetOperation __result, string assetBundleName, string assetName, Type type, string manifestAssetBundleName)
-		{
-		    __result = ResourceRedirector.HandleAsset(assetBundleName, assetName, type, manifestAssetBundleName, ref __result);
+        [HarmonyPrefix, HarmonyPatch(typeof(AssetBundleManager), nameof(AssetBundleManager.LoadAsset), new[] { typeof(string), typeof(string), typeof(Type), typeof(string) })]
+        public static bool LoadAssetPreHook(ref AssetBundleLoadAssetOperation __result, string assetBundleName, string assetName, Type type, string manifestAssetBundleName)
+        {
+            __result = ResourceRedirector.HandleAsset(assetBundleName, assetName, type, manifestAssetBundleName, ref __result);
 
             if (__result != null)
-		        return false;
+                return false;
 
-		    return true;
-		}
+            return true;
+        }
 
-		[HarmonyPostfix, HarmonyPatch(typeof(AssetBundleManager), "LoadAssetBundle", new[] {typeof(string), typeof(bool), typeof(string)})]
-		public static void LoadAssetBundlePostHook(string assetBundleName, bool isAsync, string manifestAssetBundleName)
-		{
-			//BepInLogger.Log($"{assetBundleName} : {manifestAssetBundleName} : {isAsync}");
-		}
+        [HarmonyPrefix, HarmonyPatch(typeof(AssetBundleManager), "LoadAssetAsync", new[] { typeof(string), typeof(string), typeof(Type), typeof(string) })]
+        public static bool LoadAssetAsyncPreHook(ref AssetBundleLoadAssetOperation __result, string assetBundleName, string assetName, Type type, string manifestAssetBundleName)
+        {
+            __result = ResourceRedirector.HandleAsset(assetBundleName, assetName, type, manifestAssetBundleName, ref __result);
 
-		[HarmonyPostfix,
-		 HarmonyPatch(typeof(AssetBundleManager), "LoadAssetAsync", new[] {typeof(string), typeof(string), typeof(Type), typeof(string)})]
-		public static void LoadAssetAsyncPostHook(ref AssetBundleLoadAssetOperation __result, string assetBundleName, string assetName, Type type, string manifestAssetBundleName)
-		{
-			//BepInLogger.Log($"{assetBundleName} : {assetName} : {type.FullName} : {manifestAssetBundleName ?? ""}", true);
+            if (__result != null)
+                return false;
 
-			__result = ResourceRedirector.HandleAsset(assetBundleName, assetName, type, manifestAssetBundleName, ref __result);
-		}
+            return true;
+        }
         #endregion
-	}
+    }
 }
