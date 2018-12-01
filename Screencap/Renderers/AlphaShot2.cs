ce9ffc22b1f6c8776d9c6a9f25a6e82d61ad0ae3
exdownloader <exdownloader@gmail.com>
2018-12-01 19:01:54 +0000

    Move Shader.PropertyToID from ctor to awake

--- Screencap/Renderers/AlphaShot2.cs
+++ Screencap/Renderers/AlphaShot2.cs
@@ -7,12 +7,13 @@ namespace alphaShot
 {
     public class AlphaShot2 : MonoBehaviour
     {
-        private Material matBlackout = null;
-        private Material matMask = null;
-        private int col = Shader.PropertyToID("_TargetColour");
+        private Material matBlackout;
+        private Material matMask;
+        private int col;
 
         void Awake()
         {
+            col = Shader.PropertyToID("_TargetColour")
             var abd = Screencap.Properties.Resources.blackout;
             var ab = AssetBundle.LoadFromMemory(abd);
             matBlackout = new Material(ab.LoadAsset<Shader>("assets/blackout.shader"));
