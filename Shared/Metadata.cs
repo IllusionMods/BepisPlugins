cce374d68c930242efe9ffbfee2e539ef4ce5bdd
MarC0 <dummyaccx@gmail.com>
2019-02-22 20:53:55 +0100

    Version up to 8.0

--- Shared/Metadata.cs
+++ Shared/Metadata.cs
@@ -6,7 +6,7 @@
         /// Version number used be all plugins. Must be convertible to <see cref="System.Version"/>
         /// Use ReleaseVersion.HotfixVersion format
         /// </summary>
-        public const string PluginsVersion = "7.1";
+        public const string PluginsVersion = "8.0";
         public const string CompanyName = "BepisPlugins";
     }
 }
