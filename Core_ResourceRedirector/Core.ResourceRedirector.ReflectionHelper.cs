33a93c670960903178899b17d31cbf395a3e3793
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-08-26 18:13:15 -0500

    Remove ResourceRedirector

--- Core_ResourceRedirector/Core.ResourceRedirector.ReflectionHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-namespace ResourceRedirector
-{
-    public static class ReflectionHelper
-    {
-        public static T GetValue<T>(this FieldInfo info, object instance) => (T)info.GetValue(instance);
-    }
-}
