28f42e62cb2a282e6b071bf39828de9c7c0b7032
MarC0 <dummyaccx@gmail.com>
2019-11-18 14:19:32 +0100

    Remove no longer necessary dep

--- src/AI_Sideloader/AI.Sideloader.cs
+++ src/AI_Sideloader/AI.Sideloader.cs
@@ -4,7 +4,6 @@ namespace Sideloader
 {
     [BepInDependency(XUnity.ResourceRedirector.Constants.PluginData.Identifier)]
     [BepInDependency(ExtensibleSaveFormat.ExtendedSave.GUID)]
-    [BepInDependency("AI_Fix_CultureFix", BepInDependency.DependencyFlags.SoftDependency)] // todo remove when culture fix is updated to be a preloader plugin
     [BepInPlugin(GUID, PluginName, Version)]
     public partial class Sideloader : BaseUnityPlugin
     {
