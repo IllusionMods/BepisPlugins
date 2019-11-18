2edb78548b93165de576bc17e873546589c61a79
MarC0 <dummyaccx@gmail.com>
2019-11-18 14:22:02 +0100

    Restricted BGMLoader to not load in studio

--- src/EC_BGMLoader/EC.BGMLoader.cs
+++ src/EC_BGMLoader/EC.BGMLoader.cs
@@ -3,5 +3,6 @@
 namespace BGMLoader
 {
     [BepInPlugin(GUID, PluginName, Version)]
+    [BepInDependency(XUnity.ResourceRedirector.Constants.PluginData.Identifier)]
     public partial class BGMLoader : BaseUnityPlugin { }
 }
--- src/KK_BGMLoader/KK.BGMLoader.cs
+++ src/KK_BGMLoader/KK.BGMLoader.cs
@@ -3,5 +3,9 @@
 namespace BGMLoader
 {
     [BepInPlugin(GUID, PluginName, Version)]
+    [BepInDependency(XUnity.ResourceRedirector.Constants.PluginData.Identifier)]
+    [BepInProcess("Koikatu")]
+    [BepInProcess("Koikatsu Party")]
+    [BepInProcess("KoikatuVR")]
     public partial class BGMLoader : BaseUnityPlugin { }
 }
