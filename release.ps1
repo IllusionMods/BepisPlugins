360cc5dad4a48b6b4b95a1752e0ebb311cb511d5
MarC0 <dummyaccx@gmail.com>
2020-04-25 14:01:31 +0200

    Improve release script

--- release.ps1
+++ release.ps1
@@ -7,24 +7,35 @@ else {
 
 $array = @("KK", "EC", "AI", "HS", "PH")
 
-
 $copy = $dir + "\copy\BepInEx" 
 
-$ver = "r" + (Get-ChildItem -Path ($dir + "\BepInEx\") -Filter "*.dll" -Recurse -Force)[0].VersionInfo.FileVersion.ToString()
+New-Item -ItemType Directory -Force -Path ($dir + "\out")
 
+foreach ($element in $array) 
+{
+    try
+    {
+        Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
+        New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
 
-New-Item -ItemType Directory -Force -Path ($dir + "\out")  
+        Copy-Item -Path ($dir + "\BepInEx\plugins\" + $element + "_BepisPlugins") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force 
 
-foreach ($element in $array) {
+        $ver = "r" + (Get-ChildItem -Path ($copy) -Filter "*.dll" -Recurse -Force)[0].VersionInfo.FileVersion.ToString()
 
-Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
+        Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + $element + "_BepisPlugins_" + $ver + ".zip")
+    }
+    catch 
+    {
+        # retry
+        Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
+        New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
 
-Copy-Item -Path ($dir + "\BepInEx\plugins\" + $element + "_BepisPlugins") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force 
+        Copy-Item -Path ($dir + "\BepInEx\plugins\" + $element + "_BepisPlugins") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force 
 
+        $ver = "r" + (Get-ChildItem -Path ($copy) -Filter "*.dll" -Recurse -Force)[0].VersionInfo.FileVersion.ToString()
 
-Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + $element + "_BepisPlugins_" + $ver + ".zip")
-
+        Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + $element + "_BepisPlugins_" + $ver + ".zip")
+    }
 }
 
 Remove-Item -Force -Path ($dir + "\copy") -Recurse
