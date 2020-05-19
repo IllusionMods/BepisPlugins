7ae672251b89403224cfba01b46be2d94eb2b7f1
MarC0 <dummyaccx@gmail.com>
2020-05-20 00:26:04 +0200

    Version up to 14.1

--- src/Shared/Metadata.cs
+++ src/Shared/Metadata.cs
@@ -6,6 +6,6 @@
         /// Version number used be all plugins. Must be convertible to <see cref="System.Version"/>
         /// Use ReleaseVersion.HotfixVersion format
         /// </summary>
-        public const string PluginsVersion = "14.0";
+        public const string PluginsVersion = "14.1";
     }
 }
