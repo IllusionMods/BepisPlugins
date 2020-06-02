b8a126faf04d808debdf0827b79e78d7c1600f86
MarC0 <dummyaccx@gmail.com>
2020-06-02 12:55:16 +0200

    Fixed voice pitch not being unlocked properly in hs2

--- src/HS2_SliderUnlocker/HS2.SliderUnlocker.VoicePitch.cs
+++ src/HS2_SliderUnlocker/HS2.SliderUnlocker.VoicePitch.cs
@@ -15,8 +15,16 @@ namespace SliderUnlocker
             HarmonyWrapper.PatchAll(typeof(VoicePitchUnlocker));
         }
         
-        [HarmonyPrefix, HarmonyPatch(typeof(ChaFileParameter), "get_voicePitch")]
+        [HarmonyPrefix, HarmonyPatch(typeof(ChaFileParameter), nameof(ChaFileParameter.voicePitch), MethodType.Getter)]
         public static bool VoicePitchHook(ChaFileParameter __instance, ref float __result)
+        {
+            // Replace line return Mathf.Lerp(0.94f, 1.06f, this.voiceRate);
+            __result = VanillaPitchLower + __instance.voiceRate * VanillaPitchRange;
+            return false;
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(ChaFileParameter2), nameof(ChaFileParameter2.voicePitch), MethodType.Getter)]
+        public static bool VoicePitchHook2(ChaFileParameter2 __instance, ref float __result)
         {
             // Replace line return Mathf.Lerp(0.94f, 1.06f, this.voiceRate);
             __result = VanillaPitchLower + __instance.voiceRate * VanillaPitchRange;
