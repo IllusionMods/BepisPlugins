e91d3ec1fb3e1a018a1044c8b6c90c40cb62cc5d
MarC0 <dummyaccx@gmail.com>
2021-02-14 17:49:09 +0100

    Version up to 16.2.1

--- src/Shared/Metadata.cs
+++ src/Shared/Metadata.cs
@@ -6,6 +6,6 @@
         /// Version number used be all plugins. Must be convertible to <see cref="System.Version"/>
         /// Use ReleaseVersion.HotfixVersion format
         /// </summary>
-        public const string PluginsVersion = "16.2.0.2";
+        public const string PluginsVersion = "16.2.1";
     }
 }
