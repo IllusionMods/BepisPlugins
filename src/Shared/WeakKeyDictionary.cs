b047bb4243e80c13d58dd5e8a02c2f6473046bbf
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-02-23 00:03:15 -0700

    PR for Issue #125 - Extended Save Data randomly going MIA (#126)

--- src/Shared/WeakKeyDictionary.cs
+++ src/Shared/WeakKeyDictionary.cs
@@ -1,49 +1,121 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace BepisPlugins
 {
     internal class WeakKeyDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>> where TValue : class
     {
-        private readonly Dictionary<HashedWeakReference, TValue> _items;
+        private readonly Dictionary<int, List<KeyValuePair<WeakReference, TValue>>> _items;
 
-        public WeakKeyDictionary() => _items = new Dictionary<HashedWeakReference, TValue>();
+        public WeakKeyDictionary() => _items = new Dictionary<int, List<KeyValuePair<WeakReference, TValue>>> ();
 
-        public int Count => _items.Count;
+        public int Count => DoCount();
+
+        private int DoCount()
+        {
+            int count = 0;
+            foreach (List<KeyValuePair<WeakReference, TValue>> valueList in _items.Values)
+            {
+                count += valueList.Count;
+            }
+            return count;
+        }
 
         public void Set(TKey key, TValue value)
         {
-            _items[new HashedWeakReference(key)] = value;
-            PurgeDeadKeys();
+            int newReferenceHash = RuntimeHelpers.GetHashCode(key);
+            lock (_items)
+            {
+                if (_items.TryGetValue(newReferenceHash, out List<KeyValuePair<WeakReference, TValue>> references))
+                {
+                    bool found = false;
+                    for (int i = 0; i < references.Count; i++)
+                    {
+                        KeyValuePair<WeakReference, TValue> reference = references[i];
+                        if (ReferenceEquals(reference.Value, value))
+                        {
+                            references[i] = new KeyValuePair<WeakReference, TValue>(new WeakReference(key), value);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        references.Add(new KeyValuePair<WeakReference, TValue>(new WeakReference(key), value));
+                    }
+                }
+                else
+                {
+                    List<KeyValuePair<WeakReference, TValue>> newReferences = new List<KeyValuePair<WeakReference, TValue>>();
+                    newReferences.Add(new KeyValuePair<WeakReference, TValue>(new WeakReference(key), value));
+                    _items[newReferenceHash] = newReferences;
+                }
+                PurgeDeadKeys();
+            }
         }
 
         private void PurgeDeadKeys()
         {
-            // Naive O(n) key prune
-            var deadKeys = _items.Keys.Where(reference => !reference.IsAlive).ToList();
-            foreach (var reference in deadKeys)
+            List<int> deadLists = new List<int>();
+            foreach (int key in _items.Keys)
             {
-                _items.Remove(reference);
+                List<KeyValuePair<WeakReference, TValue>> deadKeys = _items[key].Where(kvp => !kvp.Key.IsAlive).ToList();
+                foreach (KeyValuePair<WeakReference, TValue> deadKey in deadKeys)
+                {
+                    _items[key].Remove(deadKey);
+                }
+                if (_items[key].Count == 0)
+                {
+                    deadLists.Add(key);
+                }
+            }
+            foreach (int deadKey in deadLists)
+            {
+                _items.Remove(deadKey);
             }
         }
 
         public TValue Get(TKey key)
         {
-            if (_items.TryGetValue(new HashedWeakReference(key), out TValue value))
+            int lookupRef = RuntimeHelpers.GetHashCode(key);
+            if (_items.TryGetValue(lookupRef, out List<KeyValuePair<WeakReference, TValue>> valueList))
             {
-                return value;
+                TValue value = null;
+                foreach (KeyValuePair<WeakReference, TValue> kvp in valueList)
+                {
+                    if (kvp.Key.IsAlive && ReferenceEquals(kvp.Key.Target, key))
+                    {
+                        value = kvp.Value;
+                    }
+                }
+                if (value != null)
+                {
+                    return value;
+                }
             }
-
             return null;
         }
 
-        public bool Contains(TKey key) => _items.TryGetValue(new HashedWeakReference(key), out TValue _);
+        public bool Contains(TKey key) => _items.TryGetValue(RuntimeHelpers.GetHashCode(key), out List<KeyValuePair<WeakReference, TValue>> list);
 
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _items
-                .Where(x => x.Key.IsAlive)
-                .Select(x => new KeyValuePair<TKey, TValue>((TKey)x.Key.Target, x.Value))
-                .GetEnumerator();
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            List<KeyValuePair<TKey, TValue>> joinSet = new List<KeyValuePair<TKey, TValue>>();
+
+            foreach (int key in _items.Keys)
+            {
+                foreach (KeyValuePair<WeakReference, TValue> kvp in _items[key])
+                {
+                    if (kvp.Key.IsAlive)
+                        joinSet.Add(new KeyValuePair<TKey, TValue>((TKey)kvp.Key.Target, kvp.Value));
+                }
+            }
+            return joinSet.GetEnumerator();
+
+        }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
