e0c8a40b34638a2cc359b9dd3f8dffbc7043b4f9
ManlyMarco <dummyaccx@gmail.com>
2021-05-30 04:10:07 +0200

    Port Screencap to KKS

---
+++ src/KKS_Screencap/KKS.ScreenshotManager.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+using BepisPlugins;
+
+namespace Screencap
+{
+    [BepInProcess(Constants.GameProcessName)]
+    //[BepInProcess(Constants.StudioProcessName)]
+    //[BepInProcess(Constants.VRProcessName)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public partial class ScreenshotManager : BaseUnityPlugin
+    {
+    }
+}
