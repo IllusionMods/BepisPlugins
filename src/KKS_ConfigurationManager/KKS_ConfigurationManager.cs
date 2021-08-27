8deea4b44d350dcf12da09fc329b604ecadb9482
2155X <13873974+Mantas-2155X@users.noreply.github.com>
2021-08-27 17:04:08 +0300

    Port ConfigurationManagerWrapper to Koikatsu Sunshine (#135)
    * Port ConfigurationManagerWrapper to Koikatsu Sunshine
    
    * Patch the UniTask method more reliably

---
+++ src/KKS_ConfigurationManager/KKS_ConfigurationManager.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using BepisPlugins;
+using HarmonyLib;
+using System.ComponentModel;
+using Cysharp.Threading.Tasks;
+using Illusion.Game;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ConfigurationManagerWrapper
+{
+    [BepInProcess(Constants.GameProcessName)]
+    //[BepInProcess(Constants.StudioProcessName)] uncomment once studio constant is set
+    [BepInDependency(ConfigurationManager.ConfigurationManager.GUID)]
+    [Browsable(false)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class ConfigurationManagerWrapper : BaseUnityPlugin
+    {
+        public const string Version = Metadata.PluginsVersion;
+        public const string GUID = "KKS_" + ConfigurationManager.ConfigurationManager.GUID;
+        public const string PluginName = "Configuration Manager wrapper for Koikatsu Sunshine";
+
+        private static ConfigurationManager.ConfigurationManager _manager;
+
+        private void Start()
+        {
+            _manager = GetComponent<ConfigurationManager.ConfigurationManager>();
+            _manager.OverrideHotkey = true;
+
+            bool mainGame = Application.productName == Constants.GameProcessName;
+            if (mainGame)
+            {
+                Harmony.CreateAndPatchAll(typeof(ConfigurationManagerWrapper));
+                //Main game is handled by the hooks, disable this plugin to prevent Update from running
+                enabled = false;
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1) && !Manager.Scene.IsNowLoadingFade)
+                _manager.DisplayingWindow = !_manager.DisplayingWindow;
+        }
+
+        private static void CreateButton()
+        {
+            var original = GameObject.Find("ConfigScene(Clone)/Canvas/Node ShortCut/ShortCutButton(Clone)");
+            if (original == null) 
+                return;
+
+            var copy = Instantiate(original, original.transform.parent);
+            copy.name = "Plugin Settings";
+            copy.GetComponentInChildren<TextMeshProUGUI>().text = copy.name;
+
+            var button = copy.GetComponentInChildren<Button>();
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(delegate
+            {
+                _manager.DisplayingWindow = !_manager.DisplayingWindow;
+                Utils.Sound.Play(SystemSE.sel);
+            });
+        }
+        
+        [HarmonyPostfix, HarmonyPatch(typeof(ConfigScene), nameof(ConfigScene.Start))]
+        private static void OnOpen(ref UniTask __result) => __result.GetAwaiter().OnCompleted(CreateButton);
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ConfigScene), nameof(ConfigScene.Unload))]
+        private static void OnClose() => _manager.DisplayingWindow = false;
+    }
+}
