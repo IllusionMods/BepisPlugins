705ba939730a319de34ee6aa56a39cc11033f0c0
ManlyMarco <dummyaccx@gmail.com>
2021-09-21 18:16:44 +0200

    Fix screencap not disabling stock studio screenshots

---
+++ src/KKS_Screencap/KKS.ScreenshotManagerHooks.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+
+namespace Screencap
+{
+    public static partial class Hooks
+    {
+        /// <summary>
+        /// Cancel the vanilla screenshot
+        /// </summary>
+        [HarmonyPrefix, HarmonyPatch(typeof(Studio.GameScreenShot), nameof(Studio.GameScreenShot.Capture))]
+        private static bool StudioCapturePreHook() => false;
+
+        [HarmonyPrefix, HarmonyPatch(typeof(Studio.GameScreenShot), nameof(Studio.GameScreenShot.CreatePngScreen))]
+        private static bool CreatePngScreenPrefix(ref int _width, ref int _height)
+        {
+            //Multiply up render resolution.
+            _width *= CardRenderRate;
+            _height *= CardRenderRate;
+            return true;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(Studio.GameScreenShot), nameof(Studio.GameScreenShot.CreatePngScreen))]
+        private static void CreatePngScreenPostfix(ref byte[] __result) => DownscaleEncoded(ref __result);
+    }
+}
