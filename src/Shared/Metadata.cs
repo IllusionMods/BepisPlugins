c930acc1aa98b78b68154e2f5c507fb4991d4f90
ManlyMarco <dummyaccx@gmail.com>
2022-10-13 02:49:51 +0200

    Version up to 19.0

--- src/Shared/Metadata.cs
+++ src/Shared/Metadata.cs
@@ -6,6 +6,6 @@
         /// Version number used be all plugins. Must be convertible to <see cref="System.Version"/>
         /// Use ReleaseVersion.HotfixVersion format
         /// </summary>
-        public const string PluginsVersion = "18.2";
+        public const string PluginsVersion = "19.0";
     }
 }
