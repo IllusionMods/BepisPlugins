0ed3f5415e6ad5521498610871ab6fa42629531d
mosirnik <mosirnik@gmail.com>
2023-07-21 16:43:44 +0900

    [ColorCorrector] Don't override bloom stength by default (Fixes #179)
    This makes the plugin effectively no-op with the default settings.

--- src/Core_ColorCorrector/Core.ColorCorrector.cs
+++ src/Core_ColorCorrector/Core.ColorCorrector.cs
@@ -18,6 +18,7 @@ namespace ColorCorrector
         public const string Version = Metadata.PluginsVersion;
 
         private ConfigEntry<bool> SaturationEnabled { get; set; }
+        private ConfigEntry<bool> OverrideBloomStrength { get; set; }
         private ConfigEntry<float> BloomStrength { get; set; }
 
         private AmplifyColorEffect _amplifyComponent;
@@ -33,8 +34,10 @@ namespace ColorCorrector
         private void Start()
         {
             SaturationEnabled = Config.Bind("Post Processing Settings", "Enable saturation filter", true, new ConfigDescription("Whether default saturation filter will be applied to the game. This setting has no effect in Studio."));
-            BloomStrength = Config.Bind("Post Processing Settings", "Bloom strength", DefaultBloomStrength, new ConfigDescription("Strength of the bloom filter. Not active in Studio, control bloom settings through the in game Scene Effects menu.", new AcceptableValueRange<float>(0f, MaxBloomStrength)));
+            OverrideBloomStrength = Config.Bind("Post Processing Settings", "Enable bloom strength override", false, new ConfigDescription("Override the strength of the bloom filter. Not active in Studio, control bloom settings through the in game Scene Effects menu."));
+            BloomStrength = Config.Bind("Post Processing Settings", "Bloom strength", DefaultBloomStrength, new ConfigDescription("Strength of the bloom filter.", new AcceptableValueRange<float>(0f, MaxBloomStrength)));
             SaturationEnabled.SettingChanged += OnSettingChanged;
+            OverrideBloomStrength.SettingChanged += OnSettingChanged;
             BloomStrength.SettingChanged += OnSettingChanged;
 
             SceneManager.sceneLoaded += LevelFinishedLoading;
@@ -48,23 +51,23 @@ namespace ColorCorrector
                 _bloomComponent = Camera.main.gameObject.GetComponent<BloomAndFlares>();
 
 #if KK || EC
-                SetEffects(SaturationEnabled.Value, BloomStrength.Value);
+                SetEffects();
 #elif KKS
                 StartCoroutine(DelayMethod(() =>
                 {
-                    SetEffects(SaturationEnabled.Value, BloomStrength.Value);
+                    SetEffects();
                 }));
 #endif
             }
         }
 
-        private void SetEffects(bool satEnabled, float bloomPower)
+        private void SetEffects()
         {
             if (_amplifyComponent != null)
-                _amplifyComponent.enabled = satEnabled;
+                _amplifyComponent.enabled = SaturationEnabled.Value;
 
-            if (_bloomComponent != null)
-                _bloomComponent.bloomIntensity = bloomPower;
+            if (_bloomComponent != null && OverrideBloomStrength.Value)
+                _bloomComponent.bloomIntensity = BloomStrength.Value;
         }
 
         private IEnumerator DelayMethod(Action action)
@@ -73,6 +76,6 @@ namespace ColorCorrector
             action();
         }
 
-        private void OnSettingChanged(object sender, System.EventArgs e) => SetEffects(SaturationEnabled.Value, BloomStrength.Value);
+        private void OnSettingChanged(object sender, System.EventArgs e) => SetEffects();
     }
 }
