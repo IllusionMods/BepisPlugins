e1fa716a4c7720367e00db96a668e390f1c43ec8
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-02-19 16:30:59 +0100

    Refactor

--- src/Core_Screencap/Core.ScreenshotManager.cs
+++ src/Core_Screencap/Core.ScreenshotManager.cs
@@ -73,19 +73,6 @@ namespace Screencap
         public static ConfigEntry<int> UIShotUpscale { get; private set; }
 
 
-        [Flags]
-        public enum CameraGuideLinesMode
-        {
-            [Description("No guide lines")]
-            None = 0,
-            [Description("Captured area")]
-            Framing = 1 << 0,
-            [Description("Rule of thirds")]
-            RuleOfThirds = 1 << 1,
-            [Description("Golden ratio")]
-            GoldenRatio = 1 << 2,
-        }
-
         private void InitializeSettings()
         {
             KeyCapture = Config.Bind(
@@ -201,7 +188,7 @@ namespace Screencap
 
             GuideLinesModes = Config.Bind(
                 "General", "Camera guide lines",
-                CameraGuideLinesMode.Framing | CameraGuideLinesMode.RuleOfThirds,
+                CameraGuideLinesMode.Framing | CameraGuideLinesMode.GridThirds,
                 new ConfigDescription("Draws guide lines on the screen to help with framing rendered screenshots. The guide lines are not captured in the rendered screenshot.\nTo show the guide lines, open the quick access settings window.", null, "Advanced"));
 
             UIShotUpscale = Config.Bind(
@@ -524,59 +511,7 @@ namespace Screencap
         {
             if (uiShow)
             {
-                var desiredAspect = ResolutionX.Value / (float)ResolutionY.Value;
-                var screenAspect = Screen.width / (float)Screen.height;
-
-                if (screenAspect > desiredAspect)
-                {
-                    var actualWidth = Mathf.RoundToInt(Screen.height * desiredAspect);
-                    var barWidth = Mathf.RoundToInt((Screen.width - actualWidth) / 2f);
-
-                    if ((GuideLinesModes.Value & CameraGuideLinesMode.Framing) != 0)
-                    {
-                        IMGUIUtils.DrawTransparentBox(new Rect(0, 0, barWidth, Screen.height));
-                        IMGUIUtils.DrawTransparentBox(new Rect(Screen.width - barWidth, 0, barWidth, Screen.height));
-                    }
-
-                    if ((GuideLinesModes.Value & CameraGuideLinesMode.RuleOfThirds) != 0)
-                        DrawGuides(barWidth, 0, actualWidth, Screen.height, 0.3333333f);
-
-                    if ((GuideLinesModes.Value & CameraGuideLinesMode.GoldenRatio) != 0)
-                        DrawGuides(barWidth, 0, actualWidth, Screen.height, 0.236f);
-                }
-                else
-                {
-                    var actualHeight = Mathf.RoundToInt(Screen.width / desiredAspect);
-                    var barHeight = Mathf.RoundToInt((Screen.height - actualHeight) / 2f);
-
-                    if ((GuideLinesModes.Value & CameraGuideLinesMode.Framing) != 0)
-                    {
-                        IMGUIUtils.DrawTransparentBox(new Rect(0, 0, Screen.width, barHeight));
-                        IMGUIUtils.DrawTransparentBox(new Rect(0, Screen.height - barHeight, Screen.width, barHeight));
-                    }
-
-                    if ((GuideLinesModes.Value & CameraGuideLinesMode.RuleOfThirds) != 0)
-                        DrawGuides(0, barHeight, Screen.width, actualHeight, 0.3333333f);
-
-                    if ((GuideLinesModes.Value & CameraGuideLinesMode.GoldenRatio) != 0)
-                        DrawGuides(0, barHeight, Screen.width, actualHeight, 0.236f);
-                }
-
-                void DrawGuides(int offsetX, int offsetY, int viewportWidth, int viewportHeight, float centerRatio)
-                {
-                    var sideRatio = (1 - centerRatio) / 2;
-                    var secondRatio = sideRatio + centerRatio;
-
-                    var firstx = offsetX + viewportWidth * sideRatio;
-                    var secondx = offsetX + viewportWidth * secondRatio;
-                    IMGUIUtils.DrawTransparentBox(new Rect(Mathf.RoundToInt(firstx), offsetY, 1, viewportHeight));
-                    IMGUIUtils.DrawTransparentBox(new Rect(Mathf.RoundToInt(secondx), offsetY, 1, viewportHeight));
-
-                    var firsty = offsetY + viewportHeight * sideRatio;
-                    var secondy = offsetY + viewportHeight * secondRatio;
-                    IMGUIUtils.DrawTransparentBox(new Rect(offsetX, Mathf.RoundToInt(firsty), viewportWidth, 1));
-                    IMGUIUtils.DrawTransparentBox(new Rect(offsetX, Mathf.RoundToInt(secondy), viewportWidth, 1));
-                }
+                DrawGuideLines();
 
                 IMGUIUtils.DrawSolidBox(uiRect);
                 uiRect = GUILayout.Window(uiWindowHash, uiRect, WindowFunction, "Screenshot settings");
@@ -733,12 +668,12 @@ namespace Screencap
                         if (GUI.changed) GuideLinesModes.Value = val ? GuideLinesModes.Value | CameraGuideLinesMode.Framing : GuideLinesModes.Value & ~CameraGuideLinesMode.Framing;
 
                         GUI.changed = false;
-                        val = GUILayout.Toggle((GuideLinesModes.Value & CameraGuideLinesMode.RuleOfThirds) != 0, "3rds");
-                        if (GUI.changed) GuideLinesModes.Value = val ? GuideLinesModes.Value | CameraGuideLinesMode.RuleOfThirds : GuideLinesModes.Value & ~CameraGuideLinesMode.RuleOfThirds;
+                        val = GUILayout.Toggle((GuideLinesModes.Value & CameraGuideLinesMode.GridThirds) != 0, "3rds");
+                        if (GUI.changed) GuideLinesModes.Value = val ? GuideLinesModes.Value | CameraGuideLinesMode.GridThirds : GuideLinesModes.Value & ~CameraGuideLinesMode.GridThirds;
 
                         GUI.changed = false;
-                        val = GUILayout.Toggle((GuideLinesModes.Value & CameraGuideLinesMode.GoldenRatio) != 0, "Phi");
-                        if (GUI.changed) GuideLinesModes.Value = val ? GuideLinesModes.Value | CameraGuideLinesMode.GoldenRatio : GuideLinesModes.Value & ~CameraGuideLinesMode.GoldenRatio;
+                        val = GUILayout.Toggle((GuideLinesModes.Value & CameraGuideLinesMode.GridPhi) != 0, "Phi");
+                        if (GUI.changed) GuideLinesModes.Value = val ? GuideLinesModes.Value | CameraGuideLinesMode.GridPhi : GuideLinesModes.Value & ~CameraGuideLinesMode.GridPhi;
                     }
                     GUILayout.EndHorizontal();
                 }
@@ -752,7 +687,77 @@ namespace Screencap
 
                 GUI.DragWindow();
             }
-            #endregion
         }
+
+        private static void DrawGuideLines()
+        {
+            var desiredAspect = ResolutionX.Value / (float)ResolutionY.Value;
+            var screenAspect = Screen.width / (float)Screen.height;
+
+            if (screenAspect > desiredAspect)
+            {
+                var actualWidth = Mathf.RoundToInt(Screen.height * desiredAspect);
+                var barWidth = Mathf.RoundToInt((Screen.width - actualWidth) / 2f);
+
+                if ((GuideLinesModes.Value & CameraGuideLinesMode.Framing) != 0)
+                {
+                    IMGUIUtils.DrawTransparentBox(new Rect(0, 0, barWidth, Screen.height));
+                    IMGUIUtils.DrawTransparentBox(new Rect(Screen.width - barWidth, 0, barWidth, Screen.height));
+                }
+
+                if ((GuideLinesModes.Value & CameraGuideLinesMode.GridThirds) != 0)
+                    DrawGuides(barWidth, 0, actualWidth, Screen.height, 0.3333333f);
+
+                if ((GuideLinesModes.Value & CameraGuideLinesMode.GridPhi) != 0)
+                    DrawGuides(barWidth, 0, actualWidth, Screen.height, 0.236f);
+            }
+            else
+            {
+                var actualHeight = Mathf.RoundToInt(Screen.width / desiredAspect);
+                var barHeight = Mathf.RoundToInt((Screen.height - actualHeight) / 2f);
+
+                if ((GuideLinesModes.Value & CameraGuideLinesMode.Framing) != 0)
+                {
+                    IMGUIUtils.DrawTransparentBox(new Rect(0, 0, Screen.width, barHeight));
+                    IMGUIUtils.DrawTransparentBox(new Rect(0, Screen.height - barHeight, Screen.width, barHeight));
+                }
+
+                if ((GuideLinesModes.Value & CameraGuideLinesMode.GridThirds) != 0)
+                    DrawGuides(0, barHeight, Screen.width, actualHeight, 0.3333333f);
+
+                if ((GuideLinesModes.Value & CameraGuideLinesMode.GridPhi) != 0)
+                    DrawGuides(0, barHeight, Screen.width, actualHeight, 0.236f);
+            }
+
+            void DrawGuides(int offsetX, int offsetY, int viewportWidth, int viewportHeight, float centerRatio)
+            {
+                var sideRatio = (1 - centerRatio) / 2;
+                var secondRatio = sideRatio + centerRatio;
+
+                var firstx = offsetX + viewportWidth * sideRatio;
+                var secondx = offsetX + viewportWidth * secondRatio;
+                IMGUIUtils.DrawTransparentBox(new Rect(Mathf.RoundToInt(firstx), offsetY, 1, viewportHeight));
+                IMGUIUtils.DrawTransparentBox(new Rect(Mathf.RoundToInt(secondx), offsetY, 1, viewportHeight));
+
+                var firsty = offsetY + viewportHeight * sideRatio;
+                var secondy = offsetY + viewportHeight * secondRatio;
+                IMGUIUtils.DrawTransparentBox(new Rect(offsetX, Mathf.RoundToInt(firsty), viewportWidth, 1));
+                IMGUIUtils.DrawTransparentBox(new Rect(offsetX, Mathf.RoundToInt(secondy), viewportWidth, 1));
+            }
+        }
+
+        [Flags]
+        public enum CameraGuideLinesMode
+        {
+            [Description("No guide lines")]
+            None = 0,
+            [Description("Cropped area")]
+            Framing = 1 << 0,
+            [Description("Rule of thirds")]
+            GridThirds = 1 << 1,
+            [Description("Golden ratio")]
+            GridPhi = 1 << 2,
+        }
+        #endregion
     }
 }
