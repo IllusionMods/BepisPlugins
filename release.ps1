20670d04f8488a65e0641608bf2a3c6804f2557d
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-07-22 11:52:16 +0200

    Update release script

--- release.ps1
+++ release.ps1
@@ -1,5 +1,3 @@
-$array = @("KK", "EC", "AI", "HS", "HS2", "PH", "KKS", "HC")
-
 if ($PSScriptRoot -match '.+?\\bin\\?') {
     $dir = $PSScriptRoot + "\"
 }
@@ -9,36 +7,57 @@ else {
 
 $copy = $dir + "\copy\BepInEx" 
 Remove-Item -Force -Path ($copy) -Recurse -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
+New-Item -ItemType Directory -Force -Path ($copy + "\plugins") | Out-Null
 
-New-Item -ItemType Directory -Force -Path ($dir + "\out")
+New-Item -ItemType Directory -Force -Path ($dir + "\out") | Out-Null
 
 function CreateZip ($element)
 {
-    Remove-Item -Force -Path ($copy) -Recurse
-    New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
+    Write-Output ("Processing " + $element)
 
-    Copy-Item -Path ($dir + "\BepInEx\plugins\" + $element + "_BepisPlugins") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force 
+    Remove-Item -Force -Path ($copy) -Recurse
+    New-Item -ItemType Directory -Force -Path ($copy + "\plugins") | Out-Null
+
+    Copy-Item -Path ($dir + "\BepInEx\plugins\" + $element + "_BepisPlugins") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force  | Out-Null
     
-    Copy-Item -Path ($dir + "\..\README.md") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force
-    Copy-Item -Path ($dir + "\..\LICENSE") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force
+    Copy-Item -Path ($dir + "\..\README.md") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force | Out-Null
+    Copy-Item -Path ($dir + "\..\LICENSE") -Destination ($copy + "\plugins\" + $element + "_BepisPlugins") -Recurse -Force | Out-Null
 
     try
     {
-        $ver = "r" + (Get-ChildItem -Path ($copy + "\plugins") -Filter ($element + "_*.dll") -Recurse -Force)[0].VersionInfo.FileVersion.ToString() -replace "([\d+\.]+?\d+)[\.0]*$", '${1}'
+        $pluginFiles = (Get-ChildItem -Path ($copy + "\plugins") -Filter ($element + "_*.dll") -Recurse -Force)
+        if($pluginFiles.Length.Equals(0))
+        {
+            $pluginFiles = (Get-ChildItem -Path ($copy + "\plugins") -Filter ("*Sideloader.dll") -Recurse -Force)
+        }
+        $pluginFile = $pluginFiles[0]
+
+        $ver = "r" + $pluginFile.VersionInfo.FileVersion.ToString() -replace "([\d+\.]+?\d+)[\.0]*$", '${1}'
+        if(!$ver.Contains("."))
+        {
+            $ver = $ver + ".0"
+        }
     }
     catch 
-    {
-        $ver = "r" + (Get-ChildItem -Path ($copy) -Filter ("*.dll") -Recurse -Force)[0].VersionInfo.FileVersion.ToString()
+    {   
+        Write-Warning ("Failed to extract version number - " + $_)
+        $pluginFile = (Get-ChildItem -Path ($copy) -Filter ("*.dll") -Recurse -Force)[0]
+        $ver = "r" + $pluginFile.VersionInfo.FileVersion.ToString()
     }
+
+    Write-Output ("Version " + $ver + " extracted from " + $pluginFile)
     
-    & robocopy ($dir + "\BepInEx\patchers\") ($copy + "\patchers") ($element + "_*.*") /R:5 /W:5 
+    & robocopy ($dir + "\BepInEx\patchers\") ($copy + "\patchers") ($element + "_*.*") /R:5 /W:5 /nfl /ndl /njh /njs /ns /nc /np
 
     Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + $element + "_BepisPlugins_" + $ver + ".zip")
 }
 
+$array = Get-ChildItem -Path ($dir + "\BepInEx\plugins\") -Filter "*_BepisPlugins"
+
 foreach ($element in $array) 
 {
+    $element = $element.Name.Substring(0, $element.Name.IndexOf("_"))
+
     try
     {
         CreateZip ($element)
