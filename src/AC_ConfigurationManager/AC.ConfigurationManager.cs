9eb35fb5b9b55fe31fabb37eaf795cb8a14079a1
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-09-27 21:28:18 +0200

    Fix guid

--- src/AC_ConfigurationManager/AC.ConfigurationManager.cs
+++ src/AC_ConfigurationManager/AC.ConfigurationManager.cs
@@ -24,7 +24,7 @@ namespace ConfigurationManagerWrapper
     public class ConfigurationManagerWrapper : BasePlugin
     {
         public const string Version = Metadata.PluginsVersion;
-        public const string GUID = "HC_" + ConfigurationManager.ConfigurationManager.GUID;
+        public const string GUID = "AC_" + ConfigurationManager.ConfigurationManager.GUID;
         public const string PluginName = "Configuration Manager wrapper for HoneyCome";
 
         // localization table
